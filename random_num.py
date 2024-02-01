4274c49373e9dac1e22ba0cb33993fa4db1ff3ce
NeerajYadav <158468485+NeerajYadavX@users.noreply.github.com>
2024-02-01 17:39:44 +0530

    random_num.py
    number guessing game

--- random_num.py
+++ random_num.py
@@ -10,9 +10,7 @@ while not game_over:
     else:
         if number<winning_number:
             print("too low")
-            guess+=1
-            number=int(input("guess again: "))
         else:
             print("too high")
-            guess+=1
-            number=int(input("guess again: "))
+    guess+=1
+    number=int(input("guess again: "))
