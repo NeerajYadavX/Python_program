f7e889d13046c92bf167352beeb5c44d17ef1694
NeerajYadav <158468485+NeerajYadavX@users.noreply.github.com>
2024-02-01 17:34:09 +0530

    Add files via upload

---
+++ random_num.py
@@ -0,0 +1,18 @@
+import random
+winning_number=random.randint(1,100)
+guess=1
+number=int(input("Guess a random number from 1 to 100: "))
+game_over=False
+while not game_over:
+    if number==winning_number:
+        print(f"YOU WIN !!!\nyou guessed this number in {guess} times\nnumber is {winning_number} ")
+        game_over=True
+    else:
+        if number<winning_number:
+            print("too low")
+            guess+=1
+            number=int(input("guess again: "))
+        else:
+            print("too high")
+            guess+=1
+            number=int(input("guess again: "))
